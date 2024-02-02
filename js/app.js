fc593fb5c02876d9e0ad9afd9b826569957d21b7
Denis Dinkov <denisdinkov7@gmail.com>
2024-02-02 17:18:41 +0200

    Adding effects

--- js/app.js
+++ js/app.js
@@ -5,22 +5,18 @@ let xValue = 0,
 
 let rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-  xValue = e.clientX - window.innerWidth / 2;
-  yValue = e.clientY - window.innerHeight / 2;
-
-  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
-
+function update(cursorPosition) {
   parallax_el.forEach((el) => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
     let speedz = el.dataset.speedz;
     let rotateSpeed = el.dataset.rotation;
+
     let isInLeft =
       parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
 
     let zValue =
-      (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+      (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
     el.style.transform = `
     perspective(2300px)
@@ -30,4 +26,42 @@ window.addEventListener("mousemove", (e) => {
     translateY(calc(-50% + ${yValue * speedy}px))
     `;
   });
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+  xValue = e.clientX - window.innerWidth / 2;
+  yValue = e.clientY - window.innerHeight / 2;
+
+  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+  update(e.clientX);
 });
+
+// GSAP Animation
+let timeline = gsap.timeline();
+Array.from(parallax_el)
+  .filter((el) => !el.classList.contains("text"))
+  .forEach((el) => {
+    timeline.from(
+      el,
+      {
+        top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+        duration: 3.5,
+        ease: "power3.out",
+      },
+      "1"
+    );
+  });
+
+timeline.from(
+  ".text h1",
+  {
+    y:
+      window.innerHeight -
+      document.querySelector(".text h1").getBoundingClientRect().top,
+    duration: 2,
+  },
+  "2.5"
+);
